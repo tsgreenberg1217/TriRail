65a797e023b17bbbbaa8af108665c7669e765202
Todd Greenberg <tsgreenberg1217@gmail.com>
2022-04-26 23:58:34 -0400

    made scrollable viewpager

--- eta_info/src/main/java/com/tsgreenberg/eta_info/ui/EtaScreen.kt
+++ eta_info/src/main/java/com/tsgreenberg/eta_info/ui/EtaScreen.kt
@@ -1,12 +1,24 @@
 package com.tsgreenberg.eta_info.ui
 
+import android.graphics.RectF
 import android.util.Log
+import androidx.compose.foundation.Canvas
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -14,10 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.rememberScalingLazyListState
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.HorizontalPagerIndicator
-import com.google.accompanist.pager.rememberPagerState
+import com.google.accompanist.pager.*
 import com.tsgreenberg.core.navigation.TriRailNav
 import com.tsgreenberg.eta_info.EnRouteInfo
 import com.tsgreenberg.eta_info.models.EtaStationState
@@ -29,6 +38,7 @@ import com.tsgreenberg.eta_info.testing.TestingTags.ETA_VIEWPAGER
 import com.tsgreenberg.eta_info.testing.TestingTags.NO_TRAIN_NORTH
 import com.tsgreenberg.eta_info.testing.TestingTags.NO_TRAIN_SOUTH
 import com.tsgreenberg.ui_components.TriRailScaffold
+import com.tsgreenberg.ui_components.ViewPagerScroll
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -36,6 +46,7 @@ fun EtaScreen(
     triRailNav: TriRailNav,
     state: EtaStationState
 ) {
+    val items = 2
     Log.d("WEAROS_TEST", "recomp in screen")
     val scalingLazyListState = rememberScalingLazyListState()
     TriRailScaffold(
@@ -43,32 +54,44 @@ fun EtaScreen(
         state.progressBarState
     ) {
 
+        val scrollState = rememberScrollableState {
+            println(it)
+            it
+        }
         state.eta?.let {
             val enRouteMap = it.etaMap
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                val pagerState = rememberPagerState()
-                HorizontalPager(
+            val pagerState = rememberPagerState()
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .scrollable(scrollState, orientation = Orientation.Vertical),
+                contentAlignment = Alignment.Center
+            ) {
+                ViewPagerScroll(pagerState = pagerState)
+                VerticalPager(
                     enRouteMap.size,
                     state = pagerState,
                     modifier = Modifier
                         .fillMaxSize()
-                        .testTag(ETA_VIEWPAGER)
+                        .testTag(ETA_VIEWPAGER),
+                    userScrollEnabled = true
                 ) { page ->
                     val key = if (page == 0) "South" else "North"
                     Column {
                         Spacer(modifier = Modifier.padding(16.dp))
 
-                        Text(
-                            modifier = Modifier
-                                .background(Color(0xFF4E90A6))
-                                .fillMaxWidth()
-                                .testTag(if (key == "North") ETA_TITLE_NORTH else ETA_TITLE_SOUTH),
-                            text = "${key}bound",
-                            style = TextStyle(
-                                color = Color.White,
-                                textAlign = TextAlign.Center
-                            )
-                        )
+//                        Text(
+//                            modifier = Modifier
+//                                .background(Color(0xFF4E90A6))
+//                                .fillMaxWidth()
+//                                .testTag(if (key == "North") ETA_TITLE_NORTH else ETA_TITLE_SOUTH),
+//                            text = "${key}bound",
+//                            style = TextStyle(
+//                                color = Color.White,
+//                                textAlign = TextAlign.Center
+//                            )
+//                        )
 
                         enRouteMap[key]?.let {
                             if (it.isEmpty()) {
@@ -96,15 +119,6 @@ fun EtaScreen(
                     }
                 }
 
-                if (pagerState.pageCount > 0) {
-                    HorizontalPagerIndicator(
-                        modifier = Modifier
-                            .align(Alignment.BottomCenter)
-                            .padding(10.dp),
-                        pagerState = pagerState,
-                        activeColor = Color.White,
-                    )
-                }
             }
         }
 
---
+++ ui_components/src/main/java/com/tsgreenberg/ui_components/VIewPagerScrollView.kt
@@ -0,0 +1,70 @@
+package com.tsgreenberg.ui_components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.platform.LocalContext
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun ViewPagerScroll(
+    pagerState: PagerState
+){
+
+    val scrollPosition = (pagerState.currentPage + pagerState.currentPageOffset)
+        .coerceIn(0f, (pagerState.pageCount - 1).coerceAtLeast(0).toFloat())
+
+    Canvas(
+        Modifier
+            .fillMaxSize()
+            .rotate(-45f)
+    ) {
+        val stokeWidth = 10f
+        val stroke = Stroke(
+            width = stokeWidth,
+            cap = StrokeCap.Round
+        )
+        val sizeArc = Size(size.width - stokeWidth, size.height - stokeWidth)
+        val pos = Offset(
+            (size.width - sizeArc.width) / 2f,
+            (size.height - sizeArc.height) / 2f
+        )
+
+        val sweep = 90f
+
+        drawArc(
+            color = Color.White,
+            topLeft = pos,
+            style = stroke,
+            useCenter = false,
+            startAngle = 0f,
+            sweepAngle = sweep,
+            size = sizeArc,
+            alpha = .4f
+        )
+        val maxRotation = sweep - (sweep / pagerState.pageCount)
+        rotate(maxRotation * scrollPosition) {
+            drawArc(
+                color = Color.White,
+                topLeft = pos,
+                style = stroke,
+                useCenter = false,
+                startAngle = 0f,
+                sweepAngle = sweep / pagerState.pageCount,
+                size = sizeArc,
+            )
+        }
+
+    }
+
+}
