3e582ecc0193185309243d6745fded8035fc0e71
Todd Greenberg <tsgreenberg1217@gmail.com>
2022-10-07 18:45:34 -0400

    implemented gradleportal and refactored plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    id ("com.android.application") version "7.3.0" apply false
+    id ("com.android.library") version "7.3.0" apply false
+    id ("org.jetbrains.kotlin.android") version "1.7.10" apply false
+}
+
+
 buildscript {
     repositories {
         google()
@@ -6,8 +14,6 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
         classpath("com.google.gms:google-services:4.3.14")
