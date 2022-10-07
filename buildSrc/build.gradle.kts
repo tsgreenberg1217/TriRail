890f0c7b7bde2b847f6ca89574e9fa090592d748
Todd Greenberg <tsgreenberg1217@gmail.com>
2022-10-07 19:49:15 -0400

    revert buildsrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,27 +5,6 @@ plugins {
     `kotlin-dsl`
 }
 
-buildscript {
-
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-//        classpath("com.android.tools.build:gradle:7.2.2")
-//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-    }
-}
-
 repositories {
-    google()
     mavenCentral()
-}
-
-dependencies {
-//    implementation("com.android.tools.build:gradle:3.4.3")
-//    implementation("com.android.tools.build:gradle-api:3.4.3")
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
-//    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
 }
