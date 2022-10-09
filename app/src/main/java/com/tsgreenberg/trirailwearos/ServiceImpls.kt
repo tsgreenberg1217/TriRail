2822b24f399a9080b61bb3314baa72a59357559b
Todd Greenberg <tsgreenberg1217@gmail.com>
2022-10-09 19:42:57 -0400

    Revert "refactoring dependancies"
    This reverts commit ac2c5084633314822a8c861e70fdb12e07c5dff8.

--- app/src/main/java/com/tsgreenberg/trirailwearos/ServiceImpls.kt
+++ app/src/main/java/com/tsgreenberg/trirailwearos/ServiceImpls.kt
@@ -8,19 +8,16 @@ import com.tsgreenberg.station_list.StationsService
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
 import javax.inject.Inject
 
 
 class StationServiceImpl(
-    private val client: HttpClient,
-    private val builder: HttpRequestBuilder
+    private val client: HttpClient
 ) : StationsService {
     override suspend fun getStops(token: String): GetStopsResponse {
-        return client.apply2Builder(builder) {
+        return client.get {
             url("${BuildConfig.API_URL}/service.php?service=get_stops")
-            method = HttpMethod.Get
+            parameter("token", "TESTING")
         }.body()
     }
 
@@ -28,8 +25,7 @@ class StationServiceImpl(
 
 
 class EtaServiceImpl(
-    private val client: HttpClient,
-    private val builder: HttpRequestBuilder
+    private val client: HttpClient
 ) : EtaService {
     override suspend fun getVehicles(
         hasETAData: Int,
@@ -42,18 +38,11 @@ class EtaServiceImpl(
     }
 
     override suspend fun getStopEtas(stopId: Int, token: String): GetStopEtaResponseDto {
-        return client.apply2Builder(builder) {
+        return client.get {
             url("${BuildConfig.API_URL}/service.php?service=get_stop_etas")
+            parameter("token", "TESTING")
             parameter("stopIDs", stopId)
-            method = HttpMethod.Get
         }.body()
     }
-}
 
-
-suspend fun HttpClient.apply2Builder(
-    b: HttpRequestBuilder,
-    block: HttpRequestBuilder.() -> Unit
-): HttpResponse {
-    return request(b.apply(block))
 }
--- app/src/main/java/com/tsgreenberg/trirailwearos/di/NetworkingModule.kt
+++ app/src/main/java/com/tsgreenberg/trirailwearos/di/NetworkingModule.kt
@@ -12,7 +12,6 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
-import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 import javax.inject.Singleton
@@ -24,13 +23,12 @@ object NetworkingModule {
 
     @Provides
     @Singleton
-    fun getStationsService(client: HttpClient, builder: HttpRequestBuilder): StationsService =
-        StationServiceImpl(client, builder)
+    fun getStationsService(client: HttpClient): StationsService =
+        StationServiceImpl(client)
 
     @Provides
     @Singleton
-    fun getEtaService(client: HttpClient, builder: HttpRequestBuilder): EtaService =
-        EtaServiceImpl(client, builder)
+    fun getEtaService(client: HttpClient): EtaService = EtaServiceImpl(client)
 
 
     @Provides
@@ -51,13 +49,6 @@ object NetworkingModule {
                 request.url.host.contains("ktor.io")
             }
         }
-
-    }
-
-    @Provides
-    @Singleton
-    fun getHttpRequestBuilder(): HttpRequestBuilder = HttpRequestBuilder().apply {
-        parameter("token", "TESTING")
     }
 
 
