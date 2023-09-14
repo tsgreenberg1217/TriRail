e46cf13ce1ba95cb13361f8be2bdefeba6996254
Todd Greenberg <tsgreenberg1217@gmail.com>
2023-09-13 23:38:02 -0400

    update to gradle 8.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.4.1")
-    }
-}
+//buildscript {
+//    repositories {
+//        google()
+//        mavenCentral()
+//    }
+//    dependencies {
+//        classpath("com.android.tools.build:gradle:8.0.2")
+//    }
+//}
 
 plugins {
     Android.apply {
@@ -28,6 +28,7 @@ plugins {
     }
 
 }
+
 
 
 tasks.create<Delete>("cleanRP") {
--- fm_stations/build.gradle.kts
+++ fm_stations/build.gradle.kts
@@ -1,19 +1,96 @@
-apply {
-    from("$rootDir/android-library-build.gradle")
-}
-plugins{
-    Kotlin.apply { id(Kotlin.kotlinSerializationPlugin) version Kotlin.version }
-
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    kotlin(Kotlin.kapt)
+    id(Hilt.plugin)
+    Kotlin.apply { id(kotlinSerializationPlugin) version version }
     id(Sqldelight.plugin)
 }
-dependencies {
-    Modules.run{
-        "implementation"(project(core))
-        "implementation"(project(uiComponents))
+
+
+
+android {
+    compileSdk = 33
+    namespace = "com.tsgreenberg.station_list"
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
-    "implementation"(Kotlin.kotlinSerialization)
-    "implementation"(Sqldelight.driver)
-    "implementation"(AndroidX.splashScreen)
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Kotlin.complilerVersion
+    }
+
+}
+
+
+
+dependencies {
+    Modules.run {
+        implementation(project(core))
+        implementation(project(uiComponents))
+    }
+    implementation(Kotlin.kotlinSerialization)
+    implementation(Sqldelight.driver)
+    implementation(AndroidX.splashScreen)
+
+
+    kaptAndroidTest(Hilt.compilerTest)
+    kapt(Hilt.compiler)
+    kaptTest(Hilt.compilerTest) // For instrumented tests.
+
+
+    implementation(Hilt.android)
+    implementation(Hilt.navigation)
+    androidTestImplementation(Hilt.testing) // ...with Kotlin.
+
+    implementation(Compose.activity)
+    implementation(Compose.uiTooling)
+    implementation(Compose.compiler)
+    implementation(Compose.navigation)
+    implementation(Compose.wearMaterial)
+    implementation(Compose.wearFoundation)
+    debugImplementation(Compose.uiTest)
+    androidTestImplementation(Compose.junit)
+
+    implementation(Coroutines.android)
+    testImplementation(Coroutines.androidTest)
+
+    testImplementation(Junit.test)
+
+    implementation(AndroidX.navigation)
+    androidTestImplementation(AndroidX.androidTest)
+    androidTestImplementation(AndroidX.espresso)
+    androidTestImplementation(AndroidX.testCore)
+    androidTestImplementation(AndroidX.testCoreKtx)
+    androidTestImplementation(AndroidX.testRunner)
+    androidTestImplementation(AndroidX.testRules)
+    androidTestUtil(AndroidX.testOrchestrator)
+
+    testImplementation(Mockk.mockk)
+    androidTestImplementation(Mockk.mockkAndroid)
 }
 
 sqldelight {
